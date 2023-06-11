1d8ef9ca20eab83f4f269209aeb51cfe6d0243d0
halothesleeper <artitamm8@gmail.com>
2023-06-11 17:46:45 +0200

    more styling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.querySelector('#calory_icon').src = nutritionIcon;
 //displaying the form as neccesseary
 const calculateYourMacrosButton = document.querySelector('#calculate_your_macros');
 const enterYourMacrosButton = document.querySelector('#enter_your_macros');
+const macroResultsSection = document.querySelector('.macro_results')
 
 const form1 = document.querySelector('.form1');
 const form2 = document.querySelector('.form2');
@@ -16,6 +17,7 @@ form2.style.display = 'none';
 document.querySelector('.form_selector').addEventListener('click', () => {
     if (enterYourMacrosButton.checked) {
         form1.style.display = 'none';
+        macroResultsSection.style.display = 'none';
         form2.style.display = 'block';
     } else if (calculateYourMacrosButton.checked) {
         form2.style.display = 'none';
